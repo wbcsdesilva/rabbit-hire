ef943abbdff41a99e8ae9958cb45188a6717f645
wbcsdesilva <chanirusandive666@gmail.com>
2022-11-16 08:25:05 +0530

    Completed admin login page UI

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="m-5 pb-4 bg-dark text-light rounded">
+        <form action="#">
+            <div class="row row-cols-1">
+                <div class="col text-center mb-5">
+                    <i class="bi bi-key h1 text-warning"></i>
+                </div>
+                <div class="col mb-4">
+                    <label for="adminusername" class="form-label h5 text-warning">
+                        Username:
+                    </label>
+                    <input id="adminusername" type="text" class="form-control bg-dark border-secondary text-light" placeholder="Enter your administrator username here." required>
+                </div>
+                <div class="col mb-4">
+                    <label for="adminpassword" class="form-label h5 text-warning">
+                        Password:
+                    </label>
+                    <input id="adminpassword" type="password" class="form-control bg-dark border-secondary text-light" required>
+                </div>
+                <div class="col mt-5 text-center">
+                    <button class="btn btn-warning btn-lg"><span class="fw-bold">Login</span></button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
--- src/views/AdminLoginView.vue
+++ src/views/AdminLoginView.vue
@@ -1,13 +1,44 @@
-<template lang="html">
-    <div class="container">
-        <div class="card m-5">
-            <div class="card-header"><h1>We're working on it</h1></div>
-            <div class="card-body">We're not done with this one for now :3</div>
+<template>
+
+    <!-- main section -->
+
+    <section class="p-5 text-light" style="background: linear-gradient(90deg, #8E2DE2, #4A00E0);">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-md text-center">
+                    <h1 class="fw-bold mb-5">Login as an <span class="text-warning">administrator !</span></h1>
+                    <img src="../assets/login.svg" alt="" class="img-fluid w-75 mt-2">
+                </div>
+                <div class="col-md">
+                    <div class="bg-dark rounded">
+                        <div class="row row-cols-1">
+                            <div class="col">
+                                <!-- custom login form goes here -->
+                                <LoginForm/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+    </section>
+
 </template>
 <script>
+
+import LoginForm from '@/components/LoginForm.vue'
+
 export default {
+
+  data () {
+    return {
+
+    }
+  },
+
+  components: {
+    LoginForm
+  }
 
 }
 </script>
