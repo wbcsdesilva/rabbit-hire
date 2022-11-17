392faadff7eafe49a3fd1b545cb5bc9ff0ea8ec4
wbcsdesilva <chanirusandive666@gmail.com>
2022-11-17 12:50:20 +0530

    Installed firebase and added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGUdRsL7buMst-v6EfFDcyNIzAbRj6sGM",
+  authDomain: "rabbit-hire-93e3e.firebaseapp.com",
+  projectId: "rabbit-hire-93e3e",
+  storageBucket: "rabbit-hire-93e3e.appspot.com",
+  messagingSenderId: "967847594716",
+  appId: "1:967847594716:web:fc5317e27a59a6dc42c219"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
